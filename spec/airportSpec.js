776585ce2ef6ae65d35f1403ec08a04fb2d9c70d
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-17 12:08:03 +0100

    airport lets a plane land only once

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -7,7 +7,7 @@ describe ('Airport', function() {
 
 	it('Should be able to land planes', function() {
 		airport.land(plane)
-		expect(airport.planes.length).toEqual(1)
+		expect(airport.planes).toEqual([plane])
 	});
 
 	it('Should allow planes to take off', function () {
@@ -16,8 +16,9 @@ describe ('Airport', function() {
 		expect(airport.planes.length).toEqual(0)
 	});
 
-	
-
-
+	it('should only let a plane land once', function(){
+		airport.land(plane)
+		expect(airport.land(plane)).toEqual('plane has already landed')
+	});
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,15 +1,23 @@
-function Plane () {}
+function Plane () {};
 
 function Airport () {
 this.planes = []
+};
 
-}
+function LandError(message){
+	this.name = 'LandError';
+	this.message = message || 'plane has already landed';
+};	
+
+LandError.prototype = new Error();
 
 Airport.prototype.land = function(plane) {
-	this.planes.push(plane)
+	if(this.planes.indexOf(plane) === -1)
+		this.planes.push(plane)
+	else
+		return 'plane has already landed'
 };
 
 Airport.prototype.takeOff = function(number) {
 	this.planes.pop(plane)
 };
-
